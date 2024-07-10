ef343c43a286091b64aaa6d58fb4d7fd38abd86f
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-07-10 09:30:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1 - masala
+// function fn(son) {
+//     son = son.toString();
+//     console.log(+son);
+//     console.log(son.length, "xonali son");
+// }
+// fn(12345)
+
+
+
+// 2 - masala
+// function fn(...n) {
+//    let result = n.toString("").split("").join("");
+//    let count = 0
+//    for(let i = 0; i < result.length; i++){
+//     if (result[i] >= "0" && result[i] <= "9") {
+//         count ++
+      
+//     }
+    
+//    }
+//    console.log(count, "ta son bor");
+// }
+// fn("salom123")
+
+
+
+// 3 - masala
+// function fn(...n) {
+//    let result = n.toString("").split("").join("");
+//    let count = 0
+//    for(let i = 0; i < result.length; i++){
+//     if (result[i] === "a") {
+//         count ++
+//     }
+//    }
+//    console.log(count, "ta a harfi bor");
+// }
+// fn("a harfi nechtaligini hisoblash")
+
+
+
+// 4 - masala
+
